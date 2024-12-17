7fd65316d1a4b45b5f6f1c8a1cc98759de524e17
LiaoZhongHao <liao08022040@gmail.com>
2024-12-17 12:46:33 +0800

    修改脚本，以便可以下载客户端

--- install/trojan-go_install.sh
+++ install/trojan-go_install.sh
@@ -453,13 +453,14 @@ download_install(){
 
       # 下载win客户端
       [[ ! -d "${trojan_bin_dir}/client" ]] && mkdir -p "${trojan_bin_dir}/client"
-      echo "${trojan_bin_dir}/client"
       wget --no-check-certificate -O ${trojan_bin_dir}/trojan-go-windows-amd64.zip "https://github.com/p4gefau1t/trojan-go/releases/download/v0.10.6/trojan-go-windows-amd64.zip"
       sucess_or_fail "trojan-go client 下载"
       unzip -o -d ${trojan_bin_dir}/client ${trojan_bin_dir}/trojan-go-windows-amd64.zip
       sucess_or_fail "trojan-go client 解压"
 
       [[ ! -d "${web_trojan}" ]] && mkdir -p "${web_trojan}"
+      echo 'print web_trojan'
+      echo ${web_trojan}
       cp -r ${trojan_bin_dir}/client ${web_trojan}
       sucess_or_fail "trojan-go client 复制成功"
 
